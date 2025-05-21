dff57f15494868cf3401ed6ae8dfb573d10d5778
Arne Holzke <arneholzke@gmail.com>
2025-05-21 08:48:17 +0200

    V3

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+#importieren von Funktionen
 import RPi.GPIO as GPIO
 import time
 import random
@@ -5,12 +6,14 @@ import random
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.OUT)
 x=1
+y=1
 while x<100:
-    
-    x=random.randint(1,100)
-    print(x)
+    x=random.randint(1,100) 
+    p =  (99/100)**y
+    print(x,y,p*100)
     GPIO.output(16, GPIO.HIGH)
     time.sleep(x/50)
     GPIO.output(16, GPIO.LOW)
     time.sleep(1)
-    
+    y +=1
+   
